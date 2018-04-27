a90c213ea4dcb2fb9e70a949ffa3093354faf68c
luksave <lucasfelipems@gmail.com>
2018-04-27 11:31:20 -0300

    Upd: Implementando o login

--- src/email/LoginGmail.java
+++ src/email/LoginGmail.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package email;
+
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author lucas
  */
 public class LoginGmail {
+    
+    private String user;
+    private String pass;
+   
+    LoginGmail(){
+        //TODO - Interface de login com o usuário
+        this.user = "";
+        this.pass = "";
+    
+    }    
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+    
+    
     
 }
